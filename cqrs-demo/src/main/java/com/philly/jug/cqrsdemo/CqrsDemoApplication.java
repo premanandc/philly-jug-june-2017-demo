4a923eb7c84518885174b6879167abae859b166a
Premanand C <premanandc@gmail.com>
2017-06-13 21:36:21 -0400

    Initial commit

---
+++ cqrs-demo/src/main/java/com/philly/jug/cqrsdemo/CqrsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.philly.jug.cqrsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CqrsDemoApplication.class, args);
+  }
+}
